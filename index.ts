921f9c0b3a1d47c977b67520376ea209d80596de
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-10 21:58:04 +0300

    fin

--- index.ts
+++ index.ts
@@ -2,25 +2,24 @@ console.clear();
 import { fromEvent, zip, of } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 
-const eventTime = eventName =>
+// Example 4: mouse click duration (holding time)
+// https://www.learnrxjs.io/learn-rxjs/operators/combination/zip
+
+const eventTime = (eventName) =>
   fromEvent(document, eventName).pipe(map(() => new Date()));
 
-const mouseClickDuration =
-  zip(
-    eventTime('mousedown'),
-    eventTime('mouseup')
-  ).pipe(
-    map(([start, end]) =>
-      Math.abs((start.getTime() - end.getTime())))
-  );
+const mouseClickDuration = zip(
+  eventTime('mousedown'),
+  eventTime('mouseup')
+).pipe(map(([start, end]) => Math.abs(start.getTime() - end.getTime())));
 
-const isMouseClickLongerThan = milliseconds =>
+const isMouseClickLongerThan = (milliseconds) =>
   mouseClickDuration.pipe(
-    tap(v => console.log(`click was ${v} milliseconds long`)),
-    switchMap(duration => of(duration > milliseconds))
+    tap((v) => console.log(`click was ${v} milliseconds long`)),
+    switchMap((duration) => of(duration > milliseconds))
   );
 
-isMouseClickLongerThan(1000)
-  .subscribe(v => 
-    console.log(`which is ${v ? "" : "NOT"} 
-    longer than 1000 milliseconds`));
+isMouseClickLongerThan(1000).subscribe((v) =>
+  console.log(`which is ${v ? '' : 'NOT'} 
+    longer than 1000 milliseconds`)
+);
